f802deb1f8584040f96f1539d2a478bebb5048d0
liu1410121464 <1410121464@qq.com>
2018-12-09 18:47:06 +0800

    完成度完成了登录注册

--- shoe_show/static/php/code.php
+++ shoe_show/static/php/code.php
@@ -1,31 +1,29 @@
 <?php
 	session_start();
-	header("Content-type: image/png");
-	$im = imagecreate(60,16);
-	$bg = imagecolorallocate($im, 155,100,255);
-	$white = imagecolorallocate($im, 255, 255, 255);
-	$black = imagecolorallocate($im, 0, 0, 0); 
-	
-	$number_array = range(0,9);
-	
-	$font = 'simsun.ttc';
-	$myimagecode = '';
+	$image=imagecreatetruecolor(100, 30);
+	$bgcolor=imagecolorallocate($image, 255, 255, 255);//设置背景为白色,默认为黑色
+	imagefill($image, 0, 0, $bgcolor);//填充颜色，把所有与（x，y）颜色相同的点都涂成bgcolor
+	$captch_code='';//保存验证码的变量
 	for($i=0;$i<4;$i++){
-		$str = $number_array[rand(0,9)];
-		$myimagecode = $myimagecode.$str;
-		$a = 50*$i;
-		$b = 50*($i+1);
-		imagettftext($im,20,0,mt_rand($a,$b),mt_rand(20,30),$black,$font,$str);
+		$fontsize=1500;//字体大小为6px
+		$fontcolor=imagecolorallocate($image, rand(0,120),rand(0,120),rand(0,120));
+		$data='3456789qwertyuipkjhgfdsaxcvbnmQWERTYUIPLKJHGFDSAXCVBNM';//字母数字混合字典
+		$fontcontent=substr($data, rand(0,strlen($data)),1);//取从0到末尾这一段中的随机一个字符
+		$captch_code.=$fontcontent;//加入到保存验证码的变量中
+		$x=rand(5,10)+($i*100/4);
+		$y=rand(5,10);
+		imagestring($image, $fontsize, $x, $y, $fontcontent, $fontcolor);
 	}
-	$_SESSION['thisimagecode'] = $myimagecode;
-	for($i = 0;$i<60;$i++){
-		imagesetpixel($im,mt_rand(0,60),mt_rand(0,16),$white);
+	for($i=0;$i<200;$i++){
+		$pointcolor=imagecolorallocate($image, rand(50,200),rand(50,200), rand(50,200));
+		imagesetpixel($image, rand(1,99), rand(1,29), $pointcolor);//画两百个随机位置随机颜色的点，作为干扰元素
 	}
-	
-	for($i=0;$i<4;$i++){
-		imageline($im, mt_rand(0,60),mt_rand(0,16), mt_rand(0,60), mt_rand(0,16),$black); 
+	for($i=0;$i<3;$i++){
+		$linecolor=imagecolorallocate($image, rand(80,220), rand(80,220), rand(80,220));
+		imageline($image, rand(1,99), rand(1,29), rand(1,99), rand(1,29), $linecolor);//画三条随机位置随机颜色的线
 	}
-	
-	imagepng($im);
-	imagedestroy($im);
+	$_SESSION['authcode']=strtolower($captch_code);//把生成的验证码保存到session服务器
+	header('content-type:image/png');
+	imagepng($image);
+	imagedestroy($image);
 ?>
